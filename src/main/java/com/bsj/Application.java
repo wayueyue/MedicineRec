dee358b5fb60da0f5709549d9c3be0e4313f34eb
AlitaAlice <3441425824@qq.com>
2021-03-02 23:15:27 +0800

    [ADD]初始pom及目录增加

---
+++ src/main/java/com/bsj/Application.java
@@ -0,0 +1,15 @@
+package com.bsj;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author：alita
+ * @Descreption
+ * @Date：Created in 2021/3/2 23:01
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
